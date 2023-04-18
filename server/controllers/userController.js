0adf263ca585afd31235f56f62345ed3e971c215
moviefan322 <philman202@yahoo.com>
2023-04-18 18:23:29 -0400

    User crud written

---
+++ server/controllers/userController.js
@@ -0,0 +1,74 @@
+const User = require('../models/User');
+
+module.exports = {
+
+    // get all users
+async getUsers(req, res) {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
+
+    // get one user by id
+
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId });
+            if (!user) {
+                res.status(404).json({ message: 'No user with that ID' });
+                return;
+            }
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+  // create a new user
+
+  async createUser(req, res) {
+    try {
+        const user = await User.create(req.body);
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
+
+    // update a user by id
+
+async updateUser(req, res) {
+    try {
+        const user = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        );
+        if (!user) {
+            res.status(404).json({ message: 'No user with that ID' });
+            return;
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
+
+    // delete a user by id
+
+async deleteUser(req, res) {
+    try {
+        const user = await User.findOneAndDelete({ _id: req.params.userId });
+        if (!user) {
+            res.status(404).json({ message: 'No user with that ID' });
+            return;
+        }
+        res.json({ message: 'User successfully deleted' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
+};
