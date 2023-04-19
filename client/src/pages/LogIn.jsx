b66b0645bf55293f9ea6e577999dc6c41b7e2772
moviefan322 <philman202@yahoo.com>
2023-04-19 16:14:30 -0400

    adds LOGIN_USER to mutations

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -11,47 +11,102 @@ import {
   Input,
 } from '@chakra-ui/react';
 
+import Auth from '../utils/auth';
+
+const Login = (props) => {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
+
 export default function LogIn() {
-    return(
-        <Card  sx={{  
-            margin: '8rem', 
-            backgroundColor:'#E6FFFA', 
-            boxShadow: '8px 5px 5px #B2F5EA', 
-            borderRadius: '2rem'
-            }}>
-            <CardHeader>
-                <Heading fontSize='5xl' size='md' m={8}>Log In</Heading>
-            </CardHeader>
-            <CardBody sx={{display:'flex', justifyContent:'center', alignSelf:'center'}}>
-                <Stack divider={<StackDivider />} spacing='4'>
-                    <Box>
-                        <Heading size='s' textTransform='uppercase'>
-                        Email
-                        </Heading>
-                        <Input 
-                        sx={{backgroundColor:'white',
-                        borderRadius: '1rem', 
-                        paddingLeft: '5px', 
-                        margin:'5px', 
-                        width:'fit-content'}} 
-                            placeholder='email@example.com'/>
-                    </Box>
-                    <Box mb={3}>
-                        <Heading size='s' textTransform='uppercase'>
-                        Password
-                        </Heading>
-                        <Input sx={{backgroundColor:'white',
-                        borderRadius: '1rem', 
-                        paddingLeft: '5px', 
-                        margin:'5px', 
-                        width:'fit-content'}} 
-                            type='password' placeholder='Password'/>
-                    </Box>
-                    <Button mb={5} sx= {{backgroundColor:'#319795', color:'white'}} variant='outline'>
-                        Log In
-                    </Button>
-                </Stack>
-            </CardBody>
-        </Card>
-    )
+  return (
+    <Card
+      sx={{
+        margin: '8rem',
+        backgroundColor: '#E6FFFA',
+        boxShadow: '8px 5px 5px #B2F5EA',
+        borderRadius: '2rem',
+      }}
+    >
+      <CardHeader>
+        <Heading fontSize="5xl" size="md" m={8}>
+          Log In
+        </Heading>
+      </CardHeader>
+      <CardBody
+        sx={{ display: 'flex', justifyContent: 'center', alignSelf: 'center' }}
+      >
+        <Stack divider={<StackDivider />} spacing="4">
+          <Box>
+            <Heading size="s" textTransform="uppercase">
+              Email
+            </Heading>
+            <Input
+              sx={{
+                backgroundColor: 'white',
+                borderRadius: '1rem',
+                paddingLeft: '5px',
+                margin: '5px',
+                width: 'fit-content',
+              }}
+              placeholder="email@example.com"
+            />
+          </Box>
+          <Box mb={3}>
+            <Heading size="s" textTransform="uppercase">
+              Password
+            </Heading>
+            <Input
+              sx={{
+                backgroundColor: 'white',
+                borderRadius: '1rem',
+                paddingLeft: '5px',
+                margin: '5px',
+                width: 'fit-content',
+              }}
+              type="password"
+              placeholder="Password"
+            />
+          </Box>
+          <Button
+            mb={5}
+            sx={{ backgroundColor: '#319795', color: 'white' }}
+            variant="outline"
+          >
+            Log In
+          </Button>
+        </Stack>
+      </CardBody>
+    </Card>
+  );
 }
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,17 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
+
 export const ADD_USER = gql`
   mutation addUser(
     name: String!, 
