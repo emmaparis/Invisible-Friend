1dc232e59e47b2798cd52988b9161a318a4dfb57
moviefan322 <philman202@yahoo.com>
2023-04-19 14:51:24 -0400

    adds routing for singup/login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import reactLogo from './assets/react.svg';
 import viteLogo from './assets/vite.svg';
 import { ChakraProvider } from '@chakra-ui/react';
 import './App.css';
 import Header from './components/Header';
-import Create from './pages/Create'
-import Prompt from './pages/Prompt'
+import Create from './pages/Create';
+import Prompt from './pages/Prompt';
 import SignUp from './pages/SignUp';
 import LogIn from './pages/LogIn';
 import Home from './pages/Home';
-
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -21,15 +21,19 @@ function App() {
   // const [count, setCount] = useState(0);
 
   return (
-      <ApolloProvider client={client}>
-        <ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <Router>
           <Header />
           {/* <Body /> */}
           {/* <Prompt /> */}
-          <SignUp />
-          {/* <LogIn /> */}
-        </ChakraProvider>
-      </ApolloProvider>
+          <Routes>
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<LogIn />} />
+          </Routes>
+        </Router>
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,31 +1,60 @@
 import React from 'react';
-import logo from '../assets/images/ifLogoMini.png'
-import {Flex, Box, Heading, Spacer, ButtonGroup, Button, Image } from '@chakra-ui/react'
+import logo from '../assets/images/ifLogoMini.png';
+import { Link } from 'react-router-dom';
+import {
+  Flex,
+  Box,
+  Heading,
+  Spacer,
+  ButtonGroup,
+  Button,
+  Image,
+} from '@chakra-ui/react';
 
-export default function Header(){
-    return(     
-        <header style= {{ backgroundColor: '#E6FFFA', boxShadow: '0px 2px 2px #B2F5EA', display:'flex', justifyContent:'space-between', width:'100vw', border:'0px'}}>
-            <Flex sx={{display:'flex', justifyContent:'space-between', width:'100%'}}  minWidth='max-content' alignItems='center' gap='2'>
-            <Box sx={{display:'flex', flexDirection:'row', alignItems: 'center'}}>
-                <Heading sx={{color: '#1D4044'}}>INVISIBLE</Heading>
-                <Image
-                    boxSize='30px'
-                    objectFit='cover'
-                    src={logo}
-                    alt='Invisible Friend logo.'
-                    href=''
-                />
-                <Heading sx={{color: '#1D4044'}}>FRIEND</Heading>
-            </Box>
-                 <Spacer />
-                  <ButtonGroup gap='2'>
-                    {/* {loggedIN ? :} */}
-                      <Button colorScheme='teal'>Sign Up</Button>
-                      <Button colorScheme='teal'>Log in</Button>
-                  </ButtonGroup>
-              </Flex> 
-        </header> 
-    )
+export default function Header() {
+  return (
+    <header
+      style={{
+        backgroundColor: '#E6FFFA',
+        boxShadow: '0px 2px 2px #B2F5EA',
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '100vw',
+        border: '0px',
+      }}
+    >
+      <Flex
+        sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}
+        minWidth="max-content"
+        alignItems="center"
+        gap="2"
+      >
+        <Box
+          sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}
+        >
+          <Heading sx={{ color: '#1D4044' }}>INVISIBLE</Heading>
+          <Image
+            boxSize="30px"
+            objectFit="cover"
+            src={logo}
+            alt="Invisible Friend logo."
+            href=""
+          />
+          <Heading sx={{ color: '#1D4044' }}>FRIEND</Heading>
+        </Box>
+        <Spacer />
+        <ButtonGroup gap="2">
+          {/* {loggedIN ? :} */}
+          <Link to="/signup" colorScheme="teal">
+            Sign Up
+          </Link>
+          <Link to="/login" colorScheme="teal">
+            Log in
+          </Link>
+        </ButtonGroup>
+      </Flex>
+    </header>
+  );
 }
 
 // --chakra-colors-teal-50: #E6FFFA;
