021016421ced066ced1e00df42f3856203b60b05
moviefan322 <philman202@yahoo.com>
2023-04-19 11:05:09 -0400

    adds query/mutation utils to front end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,135 +1,193 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USERS = gql`
-  query allUsers {
-    users {
+export const ADD_USER = gql`
+  mutation addUser(
+    name: String!, 
+    email: String!, 
+    password: String!) {
+    addUser(name: String!, 
+        email: String!, 
+        password: String!) {
       _id
-      username
+      name
       email
-      friends {
-        _id
-        name
-        language
-        age
-        mood
-      }
-      experts {
-        _id
-        name
-        language
-        expertise
-      }
     }
   }
 `;
 
-export const USER = gql`
-  query User($id: ID!) {
-    user(_id: $id) {
-      username
-      email
-      friends {
-        _id
-        name
-        language
-        age
-        mood
-      }
-      experts {
-        _id
-        name
-        language
-        expertise
-      }
+export const UPDATE_USER = gql`
+    mutation updateUser(
+        _id: ID!
+        name: String!
+        email: String!
+        password: String!
+    ) {
+        updateUser(
+            _id: ID!
+            name: String!
+            email: String!
+            password: String!
+        ) {
+            _id
+            name
+            email
+        }
     }
-  }
 `;
 
-export const QUERY_ME = gql`
-  query me {
-    me {
-      _id
-      username
-      email
-      friends {
-        _id
-        name
-        language
-        age
-        mood
-      }
-      experts {
-        _id
-        name
-        language
-        expertise
-      }
+export const DELETE_USER = gql`
+    mutation deleteUser(_id: ID!) {
+        deleteUser(_id: ID!) {
+            _id
+            name
+            email
+        }
     }
-  }
 `;
 
-export const QUERY_FRIENDS = gql`
-  query allFriends {
-    friends {
-      _id
-      name
-      language
-      age
-      mood
-      user {
-        _id
-        username
-        email
-      }
+export const ADD_FRIEND = gql`
+    mutation addFriend(
+        name: String!
+        language: String!
+        age: Int!
+        mood: String!
+        user: String!
+    ) {
+        addFriend(
+            name: String!
+            language: String!
+            age: Int!
+            mood: String!
+            user: String!
+        ) {
+            _id
+            name
+            language
+            age
+            mood
+            user {
+                _id
+                name
+                email
+            }
+        }
     }
-  }
 `;
 
-export const QUERY_FRIEND = gql`
-  query Friend($id: ID!) {
-    friend(_id: $id) {
-      _id
-      name
-      language
-      age
-      mood
-      user {
-        _id
-        username
-        email
-      }
+export const UPDATE_FRIEND = gql`
+    mutation updateFriend(
+        _id: ID!
+        name: String!
+        language: String!
+        age: Int!
+        mood: String!
+        user: String!
+    ) {
+        updateFriend(
+            _id: ID!
+            name: String!
+            language: String!
+            age: Int!
+            mood: String!
+            user: String!
+        ) {
+            _id
+            name
+            language
+            age
+            mood
+            user {
+                _id
+                name
+                email
+            }
+        }
     }
-  }
 `;
 
-export const QUERY_EXPERTS = gql`
-  query allExperts {
-    experts {
-      _id
-      name
-      language
-      expertise
-      user {
-        _id
-        username
-        email
-      }
+export const DELETE_FRIEND = gql`
+    mutation deleteFriend(_id: ID!) {
+        deleteFriend(_id: ID!) {
+            _id
+            name
+            language
+            age
+            mood
+            user {
+                _id
+                name
+                email
+            }
+        }
     }
-  }
 `;
 
-export const QUERY_EXPERT = gql`
-  query Expert($id: ID!) {
-    expert(_id: $id) {
-      _id
-      name
-      language
-      expertise
-      user {
-        _id
-        username
-        email
-      }
+export const ADD_EXPERT = gql`
+    mutation addExpert(
+        name: String!
+        language: String!
+        expertise: String!
+        user: String!
+    ) {
+        addExpert(
+            name: String!
+            language: String!
+            expertise: String!
+            user: String!
+        ) {
+            _id
+            name
+            language
+            expertise
+            user {
+                _id
+                name
+                email
+            }
+        }
     }
-  }
+`;
+
+export const UPDATE_EXPERT = gql`
+    mutation updateExpert(
+        _id: ID!
+        name: String!
+        language: String!
+        expertise: String!
+        user: String!
+    ) {
+        updateExpert(
+            _id: ID!
+            name: String!
+            language: String!
+            expertise: String!
+            user: String!
+        ) {
+            _id
+            name
+            language
+            expertise
+            user {
+                _id
+                name
+                email
+            }
+        }
+    }
+`;
+
+export const DELETE_EXPERT = gql`
+    mutation deleteExpert(_id: ID!) {
+        deleteExpert(_id: ID!) {
+            _id
+            name
+            language
+            expertise
+            user {
+                _id
+                name
+                email
+            }
+        }
+    }
 `;
