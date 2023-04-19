d68da7f01743603905a66d75ecfb85c76ac58704
moviefan322 <philman202@yahoo.com>
2023-04-18 20:56:15 -0400

    adds Expert model

---
+++ server/models/Expert.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const expertSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    language: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    expertise: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+});
+
+const Expert = model('Expert', expertSchema);
+
+module.exports = Expert;
