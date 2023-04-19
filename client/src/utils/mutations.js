ace72bc99a06403b4e5c5d91946e5623ea8f0171
moviefan322 <philman202@yahoo.com>
2023-04-19 19:28:40 -0400

    signup gets 200 from client-side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -15,9 +15,7 @@ export const LOGIN_USER = gql`
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
-      _id
       token
-      username
     }
   }
 `;
