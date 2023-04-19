67eebd804e2a6ec120b53fb0a4af50a7b0533729
ShannyaN <123843574+ShannyaN@users.noreply.github.com>
2023-04-19 12:18:26 -0400

    saved friend button

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button, ButtonGroup, Spacer } from '@chakra-ui/react';
+import SavedFriendButton from "../subcomponents/SavedFriendButton"
+
+function Home () {
+    return(
+        <ButtonGroup style={{marginTop:'150px', display:'flex', flexDirection: 'column', alignSelf:'center', justifyContent:'center'}} > 
+                <Button size='lg' class='createfriend' >
+                    <strong> + </strong> &emsp; Create a friend
+                </Button>
+                <SavedFriendButton/>
+                <SavedFriendButton/>
+       </ButtonGroup>
+    )
+}
+
+export default Home;
