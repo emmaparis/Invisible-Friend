5c6b1967afdfbf265dd653b406d13fce16f5ce4b
Laert Xhumari <laertxhumari@gmail.com>
2023-04-19 18:29:07 -0400

    added custom 404 page

---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div className="not-found">
+        <div className="animation-wrapper">
+          <div className="animation">
+            <img
+              id="gif-image"
+              src="/src\assets\images\404.gif"
+              alt="404 GIF"
+            />
+          </div>
+        </div>
+        <h1>404</h1>
+        <h2>Page not found!</h2>
+        <p>
+          The page you are looking for might have been removed or is temporarily
+          unavailable.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
