423476d9f248922214971bac13bdc7159add4267
Colin Sprows <colin.sprows@gmail.com>
2023-04-18 21:11:29 -0400

    friend type select bar is working but its getting late, going to style in the morning. commented out of app for now

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { Container, FormControl, FormLabel } from "@chakra-ui/react";
+import { Select } from "chakra-react-select";
+
+const friendTypeOptions = [
+    {value: "Friend", label: "Friend"},
+    {value: "Teacher", label: "Teacher"}
+]
 
 export default function Create() {
   return (
-    <div>Create</div>
+    <Container mb={16}>
+    <FormControl p={4}>
+      {/* <FormLabel>chakra-react-select demo</FormLabel> */}
+      <Select
+        name="colors"
+        classNamePrefix="Friend-Type-Select"
+        options={friendTypeOptions}
+        placeholder="Friend Type"
+        _placeholder={{ fontSize: "100px" }}
+        closeMenuOnSelect={false}
+        chakraStyles={{
+          dropdownIndicator: (prev, { selectProps: { menuIsOpen } }) => ({
+            ...prev,
+            "> svg": {
+              transitionDuration: "normal",
+              transform: `rotate(${menuIsOpen ? -180 : 0}deg)`
+            }
+          })
+        }}
+      />
+    </FormControl>
+  </Container>
   )
 }
