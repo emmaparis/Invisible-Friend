08b3c2cf1f9bc2832fdbdb9afe7f5d034367a1af
Colin Sprows <colin.sprows@gmail.com>
2023-04-21 18:05:54 -0400

    OK TAKE 2 HISTORY IS FIXED

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,8 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type User {
     _id: ID!
-    username: String!
-    email: String!
+    username: String
+    email: String
     password: String!
     friends: [Friend]
     experts: [Expert]
