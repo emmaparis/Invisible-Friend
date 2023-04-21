9ef68b12f291d34e05eb10b09620c100b631fab1
moviefan322 <philman202@yahoo.com>
2023-04-21 11:58:17 -0400

    userData rendering on profile elements

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -12,10 +12,25 @@ import {
   Button,
 } from '@chakra-ui/react';
 import { useStoreContext } from '../utils/GlobalState';
+import { QUERY_ME } from '../utils/queries';
 
 function Profile() {
   const [state, dispatch] = useStoreContext();
   const userData = state.user;
+
+  if (!userData) {
+    const { error, data } = useQuery(QUERY_ME, {
+      context: {
+        headers: {
+          Authorization: `Bearer ${Auth.getToken()}`,
+        },
+      },
+    });
+    dispatch({
+      type: UPDATE_USER,
+      user: data.me,
+    });
+  }
 
   console.log('state', userData);
   console.log(userData.username);
@@ -57,14 +72,16 @@ function Profile() {
                 Saved Friends
               </Heading>
               <Stack direction="row" justify="space-between">
-                {userData.friends.map((friend) => (
-                  <Text pt="2" fontSize="sm">
-                    {friend.name}
-                  </Text>
+                {userData?.friends?.map((friend) => (
+                  <>
+                    <Text pt="2" fontSize="sm" key={friend._id}>
+                      {friend.name}
+                    </Text>
+                    <Button variant="solid" colorScheme="red" size="sm">
+                      Delete
+                    </Button>
+                  </>
                 ))}
-                <Button variant="solid" colorScheme="teal" size="sm">
-                  Edit
-                </Button>
               </Stack>
             </Box>
             <Box>
@@ -72,12 +89,16 @@ function Profile() {
                 Saved Teachers
               </Heading>
               <Stack direction="row" justify="space-between">
-                <Text pt="2" fontSize="sm">
-                  You do not have any teachers
-                </Text>
-                <Button variant="solid" colorScheme="teal" size="sm">
-                  Edit
-                </Button>
+                {userData?.experts?.map((expert) => (
+                  <>
+                    <Text pt="2" fontSize="sm" key={expert._id}>
+                      {expert.name}
+                    </Text>
+                    <Button variant="solid" colorScheme="red" size="sm">
+                      Delete
+                    </Button>
+                  </>
+                ))}
               </Stack>
             </Box>
           </Stack>
