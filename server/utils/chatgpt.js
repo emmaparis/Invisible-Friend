f773c35d795982ba23218499d686727a3f0c7a6b
Colin Sprows <colin.sprows@gmail.com>
2023-04-22 19:07:55 -0400

    fixed teacher prompt

--- server/utils/chatgpt.js
+++ server/utils/chatgpt.js
@@ -1,9 +1,17 @@
-function generatePrompt(input, friendType, temperament, age, language) {
+function generatePrompt(input, friendType, temperament, language, age) {
+    if (friendType === "Friend") {
     return `
     Prompt: ${input}
 
     Model prompt: Respond to me like you are a ${temperament} ${friendType} that is ${age} years old and speaks ${language}
     `;
+    } else {
+    return `
+    Prompt: ${input}
+
+    Model prompt: Respond to me like you are a ${temperament} ${friendType} that speaks ${language}
+    `;
+    }
 }
 
 module.exports = {generatePrompt};
