b517ffd58efa9f1506d1ed98546861d6626be6ea
Colin Sprows <colin.sprows@gmail.com>
2023-04-23 16:44:30 -0400

    text to speech working when you click message bubble

--- client/src/subcomponents/Message.jsx
+++ client/src/subcomponents/Message.jsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSpeechSynthesis } from 'react-speech-kit';
 
 export default function Message({ role, content }) {
-  return <div className={`message ${role}`}>{content}</div>;
+
+  const [text, setText] = useState('Hello this is a test');
+  const { speak } = useSpeechSynthesis();
+
+  const handleOnClick = (text) => {
+    speak({ text: text });
+  };
+
+  return <div className={`message ${role}`} onClick={()=>{handleOnClick(content)}}>{content}</div>;
 }
